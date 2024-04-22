ea827b523c6a34adf5aa6ffe8e57006a265b1c26
dongyh <dongyh0721@163.com>
2024-04-22 14:54:47 +0800

    eslint/prettier

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    init
-  </div>
+  <div>init</div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
